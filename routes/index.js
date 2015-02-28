eb1972f2fcbf3bc3515e40209d969d2284fd343e
modou6c <contact.modoucisse@gmail.com>
2015-02-28 12:49:26 +0100

    clean and cross domain allow

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-// node mysql
-//var mysql      = require('mysql');
-//var connection  = require('express-myconnection'); 
-/*var connection = mysql.createConnection({
-  host     : 'localhost', //'db566290755.db.1and1.com',
-  user     : 'root', //'dbo566290755',
-  socketPath: 'mysql-socket-path',
-  password : 'mypass',//'505932qq',
-  port : 3306
-  //database : 'db566290755'
-});*/
-
 var app = require('../app');
 
-
-/* Get current user's rank*/ 
-/*function get_user_rank(mail,connection){
-
-	var query = 'SELECT best_score, FIND_IN_SET( best_score, ( SELECT GROUP_CONCAT( best_score' + 
-				 'ORDER BY best_score DESC ) FROM users )) AS rank FROM users WHERE mail = ?'
-
-	connection.query(query, [mail], function(err, rank){
-	if(err) {
-		console.log('Error in get rank query');
-		console.log(err);
-
-	  	//res.json({"code" : 100, "status" : "Error in get rank query"});
-	  		 return 0;
-	  	}
-	  	else {
-	  		return rank[0].rank;
-	  	 }
-
-	});
-}*/
 
 /* Home page */
 router.get('/', function(req, res) {
@@ -61,12 +28,9 @@ router.get('/classement/:mail/:score', function(req, res) {
         connection.query(strQuery,[mail], function(err,rows){
             if(err)	{
             	console.log('Error in select query ...\n\n');
-	  			//res.json({"code" : 100, "status" : "Error in select query"});
 	  			return;
 	  		}
 	  		else if (rows.length == 0) { // new user
-	  			//var newUser  = {login: mail, mail: mail, best_score: score};
-	  	
 
 	  			var insertQuery = 'INSERT INTO users(login,mail,best_score) VALUES (?,?,?) ';
 	  			var login = "log";
@@ -80,9 +44,6 @@ router.get('/classement/:mail/:score', function(req, res) {
 	  				}
 	  				else {
 	  					console.log('New User is created in the DB...\n\n');
-	  					// get user rank;s
-	  		  			//rank = get_user_rank(mail,connection);
-	  		  		//	connection.release();
 	  				}
 			});
 
@@ -106,9 +67,6 @@ router.get('/classement/:mail/:score', function(req, res) {
 
 	  		 	 });
 	  		  }
-
-	  		  // get user rank;
-	  		  //rank = get_user_rank(mail,connection);
 	  		 
 	  		}
 
@@ -128,6 +86,8 @@ router.get('/classement/:mail/:score', function(req, res) {
 			  		console.log('rank of '+ mail + ' ==>');
 			  		console.log(rank[0]);
 	  				// Send response
+	  				res.header("Access-Control-Allow-Origin", "*");
+  					res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	  				res.send(rank[0]);  
 			  	 }
 
@@ -138,29 +98,4 @@ router.get('/classement/:mail/:score', function(req, res) {
     });
 });
 
-
-//router.get('/', home);// home page, welcoming
-//router.get('/classement/:mail/:score', get_rank);//route get the user's rank 
-
-/* GET classement. */
-/*router.get('/classement/:mail/:score', function(req, res, next) {
-	connection.connect(function(err){
-		if(err) {
-			console.log(err);
-		    console.log("Error connecting database ... \n\n");
-		     
-		} else {
-
-			console.log("Database is connected ... \n\n"); 
-			handle_database(req, res); 
-		
-		}
-
-	});
-
-	connection.end(); 
-
-});*/
-	
-//var app       =    express();
 module.exports = router;
